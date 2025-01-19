1294719dc79fbf61c13fb7f69c499a96322af906
gani1990 <gani87122@gmail.com>
2025-01-19 12:43:45 +0530

    Added changes related to self modules and used community modules as well

---
+++ AWS/modules_demo/main.tf
@@ -0,0 +1,43 @@
+#Self written module
+module "myvpc" {
+  source       = "./modules/vpc"
+  vpc_cidr     = "192.168.0.0/16"
+  network_name = "vpc-module"
+  private_subnets = [{
+    name = "app"
+    cidr = "192.168.0.0/24"
+    az   = "ap-south-1a"
+  }]
+  public_subnets = [{
+    name = "web"
+    cidr = "192.168.1.0/24"
+    az   = "ap-south-1a"
+  }]
+
+}
+
+
+#Community module user here 
+module "http_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  vpc_id  = module.myvpc.vpc_id
+  name    = "web"
+  ingress_with_cidr_blocks = [{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "open http"
+    cidr_blocks = "0.0.0.0/0"
+    }, {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "open https"
+    cidr_blocks = "0.0.0.0/0"
+  }]
+}
+
+output "vpcid" {
+  value = module.myvpc.vpc_id
+}
---
+++ AWS/modules_demo/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.82.2"
+    }
+  }
+  required_version = ">= 1.10.0"
+  backend "s3" {
+    bucket         = "backendtfstate"
+    region         = "us-east-1"
+    key            = "module_demo/terraform.tfstate"
+    dynamodb_table = "backendtfstatelock"
+
+  }
+}
