205f176c1b7359171d7f1cefe1ae42948a0b456f
gani1990 <gani87122@gmail.com>
2025-01-19 12:10:33 +0530

    created vpc using s3 backend and dynamodb locking

---
+++ AWS/backend1/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+  required_version = ">= 1.10.0"
+
+  backend "s3" {
+    bucket = "backendtfstate"
+    region = "us-east-1"
+    key = "backendtest/terraform.tfstate"
+    dynamodb_table = "backendtfstatelock"
+    
+  }
+}
+
+# Configure the AWS Provide
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "base" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "hello-tf"
+  }
+
+}
